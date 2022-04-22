bd3dba38d8521ae6f946080e0c252689e87fc925
zopilotech <ant.gon.vel@gmail.com>
2022-04-22 08:25:19 -0500

    Third Commit

--- cypress/integration/basic_functions.js
+++ cypress/integration/basic_functions.js
@@ -1,21 +1,23 @@
 /// <reference types="Cypress"/>
-describe('basic Function', function() {
-    it('basic Function', function(){
+import * as storeSelector from  '../support/storeSelector'
+describe('GREEN KART, Simple Store, Add to cart and place order', function() {
+    it('Buy Carrots and Cucumbers', function(){
         //visit any web site
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
         
         //Type to a field
         cy.get('.search-keyword').type("Brocolli");
         //Type again to the fiel, the text will be chain
-        cy.get('.search-keyword').type("Ca");
+        //Using the StoreSelector.js file 
+        cy.get(storeSelector.SearchField).type("Ca");
         //Wait one second        
         cy.wait(1000);
         //Clear the field
-        cy.get('.search-keyword').clear();
+        cy.get(storeSelector.SearchField).clear();
         //Type Ca on the field
-        cy.get('.search-keyword').type("Ca");
+        cy.get(storeSelector.SearchField).type("Ca");
         //Clear the input and type a new value
-        cy.get('.search-keyword').clear().type('Carrot')
+        cy.get(storeSelector.SearchField).clear().type('Carrot')
 
         //Get Title Assertions
         cy.get('.brand').should('have.text', 'GREENKART');
@@ -25,14 +27,39 @@ describe('basic Function', function() {
         //Click on Button Several times for Carrot
         for(let n = 0; n < 10; n ++){
             cy.get(':nth-child(1) > .stepper-input > .increment')
-              .click()
-          }    
+            .click()
+        }    
 
-
+        for(let n = 0; n < 5; n ++){
+            cy.get(':nth-child(1) > .stepper-input > .decrement')
+            .click()
+        }    
+        
+        //Click Button to add Carrot
+        cy.get('.product-action > button').click();
         cy.get('.search-keyword').clear();
+        cy.wait(1000);
 
-        //cy.get('[href="#/offers"]').click();
-        //cy.get('#page-menu').select('10');
+        //Search for Cucumber
+        cy.get('.search-keyword').clear().type('Cucumber');
+        cy.wait(1000);
+
+        for(let n = 0; n < 3; n ++){
+            cy.get(':nth-child(1) > .stepper-input > .increment')
+            .click()
+        }    
+
+        cy.get('.product-action > button').click();
+        cy.get('.search-keyword').clear();
+        cy.wait(1000);
+
+
+        cy.get('.cart-icon > img').click();
+        
+        cy.get('.cart-preview > .action-block > button').click();
+      
+
+        cy.get('.discountAmt').should('have.text',768)
 
     })
   
---
+++ cypress/support/storeSelector.js
@@ -0,0 +1,3 @@
+//Main Store Page
+export const SearchField = '.search-keyword';
+export const detailpagelink = ':nth-child(2) > .nav-link > span';
